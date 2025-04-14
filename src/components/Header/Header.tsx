08435f499913b6b66d2f3e152dfe481cf2f6f030
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-14 15:50:45 +0300

    Create component Header.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <header className="header">HEADER</header>
+        </div>
+    );
+}
+ 
+export default Header;
