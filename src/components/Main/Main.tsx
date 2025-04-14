06227c761adcda0e6bc92d8d0e83520e99b20235
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-14 15:52:37 +0300

    Add changes in the Main.

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+const Main = () => {
+    return (
+        <div className="container">
+            <main className="main">MAIN</main>
+        </div>  
+    );
+}
+ 
+export default Main;
