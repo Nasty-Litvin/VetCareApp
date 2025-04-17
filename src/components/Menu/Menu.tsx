ee36d51592351c2754e100cc735c1aee4c459463
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-17 15:24:55 +0300

    Added elements in the Menu.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,21 @@
-import './style.scss'
+import menuData from '../../data/menu.json';
+import MenuItem from '../MenuItem/MenuItem';
+import './style.scss';
 
 const Menu = () => {
     return (
-        <div className="menu">MENU</div>
+        <div>
+            <nav className="menu">
+                <ul className="menu__list">
+                    {menuData.map((item) => (
+                        <MenuItem 
+                            key={item.id}
+                            text={item.text}
+                        />
+                    ))}
+                </ul>
+            </nav>
+        </div>
     );
 }
  
