53468851b718bb40bd7f5ca2e99fb64caaed4255
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-21 17:30:50 +0300

    Added components in the Main.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,7 @@
+import CallToAction from '../CallToAction/CallToAction';
+import Experience from '../Experience/Experience';
+import PricingPlan from '../PricingPlan/PricingPlan';
+import Team from '../Team/Team';
 import Work from '../Work/Work';
 
 import './style.scss'
@@ -7,6 +11,10 @@ const Main = () => {
         <div className="container">
             <main className="main">
                 <Work />
+                <Experience />
+                <Team />
+                <PricingPlan />
+                <CallToAction />
             </main>
         </div>  
     );
