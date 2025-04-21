21e0281a784793877410833ab4e0a3e5c3a19536
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-21 17:43:22 +0300

    Added style (background).

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 
-
+import './styles/index.scss'
 
 function App() {
 
