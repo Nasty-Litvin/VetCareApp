7875db2500bfd24aa06b48629d7c21cd29d02277
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-21 17:31:54 +0300

    Added cards and styles in the Team.

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,8 +1,32 @@
+import Title from '../Title/Title';
+import Card from '../Card/Card';
+import teamData from '../../data/team.json'
+
 import './style.scss';
+
+const teamTitle = 'Meet Our Groomers'
 
 const Team = () => {
     return (
-        <div className="team">TEAM</div>
+        <div className="team">
+            <div className="team__wrapper">
+                <div className="team__text">Our Team</div>
+                <div className="team__title">
+                    <Title text={teamTitle} />
+                </div>
+                <div className="team__cards">
+                    {teamData.map((item) => (
+                        <Card 
+                            key={item.id}
+                            url={item.img}
+                            name={item.name}
+                            description={item.designation}
+                            bgColor={item.color}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 }
  
