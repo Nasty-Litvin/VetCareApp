508f1e9df65421343140a5198616ac999ec12ecd
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-22 16:23:50 +0300

    Added props in the component Logo.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,12 @@
+import { LogoProps } from './types';
 import './style.scss'
 
-const Logo = () => {
+
+const Logo = ({url}: LogoProps) => {
     return (
         <div className="logo__wrapper">
             <a className="logo__link">
-                <img className="logo__img" src="/logo.svg"/>
+                <img className="logo__img" src={url}/>
             </a>
         </div>
     );
