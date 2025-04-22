ebce2540f662253fb02b5e924c5a270fbe459ef0
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-22 16:24:24 +0300

    Added in the Footer new components.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
+import About from '../About/About';
+import Address from '../Address/Address';
+import Links from '../Links/Links';
+import OpeningHours from '../OpeningHours/OpeningHours';
+
 import './style.scss'
+
+
 
 const Footer = () => {
     return (
         <div className="container">
-            <footer className="footer">FOOTER</footer>
+            <footer className="footer">
+                <div className="footer__about">
+                    <About />
+                    <Address />
+                    <Links />
+                    <OpeningHours />
+                </div>
+            </footer>
         </div>
     );
 }
